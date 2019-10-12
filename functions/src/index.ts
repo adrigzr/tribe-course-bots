a94888ffa652da8bd558fa0a3fa27eed7b8d463e
adrigzr <adrigzr@users.noreply.github.com>
2019-10-12 13:50:37 +0200

    feat: add SSML

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,47 +9,45 @@ const app = dialogflow<UserStorage, UserStorage>({
   debug: true
 });
 
+function join(strs: TemplateStringsArray, ...substs: string[]) {
+    return substs.reduce((prev,cur,i) => prev+cur+strs[i+1], strs[0]);
+}
+
+function prosody(strs: TemplateStringsArray, ...substs: string[]): string {
+    return `
+	<speak>
+	    <prosody rate="120%" pitch="2st">${join(strs, ...substs)}</prosody>
+	</speak>`;
+}
+
 app.intent('Default Welcome Intent', conv => {
-    conv.ask(`
-<speak>
-    <p>
-	<s><emphasis level="strong">!Hola!</emphasis></s>
-	<s>Soy tu gestor de colores.</s>
-	<s>¿Qué quieres hacer?</s>
-  </p>
-</speak>`);
+    conv.ask(prosody`¡Hola! Soy tu gestor de colores. ¿Qué quieres hacer?`);
 });
 
 app.intent('help', conv => {
-    conv.ask(`
-<speak>
-    <p>
-	<s>Puedo ayudarte a guardar tu color favorito y decírtelo cuando lo necesites.</s>
-	<s>¿Qué quieres hacer?</s>
-    </p>
-</speak>`);
+    conv.ask(prosody`Puedo ayudarte a guardar tu color favorito y decírtelo cuando lo necesites. ¿Qué quieres hacer?`);
 });
 
 app.intent('save', (conv, { color }) => {
     if (!color || typeof color !== 'string') {
-	conv.ask('Necesito que me especifiques un color.');
+	conv.ask(prosody`Necesito que me especifiques un color.`);
 	return;
     }
 
     conv.user.storage.color = color;
 
-    conv.ask(`¡Vale! He guardado el ${color} como tu color favorito.`);
+    conv.ask(prosody`¡Vale! He guardado el ${color} como tu color favorito.`);
 });
 
 app.intent('get', conv => {
     const color = conv.user.storage.color;
 
     if (!color) {
-	conv.ask('No has guardado ningún color. ¿Qué color quieres guardar?');
+	conv.ask(prosody`No has guardado ningún color. ¿Qué color quieres guardar?`);
 	return;
     }
 
-    conv.ask(`Tu color favorito es el ${color}.`);
+    conv.ask(prosody`Tu color favorito es el ${color}.`);
 });
 
 app.intent('get.save', (conv, { color }) => {
@@ -65,8 +63,8 @@ app.intent('confirmation_help', (conv, _, granted) => {
 });
 
 app.fallback(conv => {
-    conv.ask('Perdona, no te he entendido.');
-    conv.ask(new Confirmation('¿Quieres ver la ayuda?'));
+    conv.ask(prosody`Perdona, no te he entendido.`);
+    conv.ask(new Confirmation(prosody`¿Quieres ver la ayuda?`));
 });
 
 export const conversation = functions.region('europe-west1').https.onRequest(app);
